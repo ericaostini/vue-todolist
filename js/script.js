200706f7efbcadb10ec97010a5f1453b9f328612
ericaostini <ericaostini14@gmail.com>
2023-10-31 15:23:46 +0100

    array-object creation

--- js/script.js
+++ js/script.js
@@ -3,6 +3,28 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+            tasks: [
+                {
+                    id: 1,
+                    text: "Fare i compiti",
+                    done: true
+                },
+                {
+                    id: 2,
+                    text: "Fare la spesa",
+                    done: false
+                },
+                {
+                    id: 3,
+                    text: "Ritirare la posta",
+                    done: false
+                },
+                {
+                    id: 4,
+                    text: "Svuotare la lavatrice",
+                    done: true
+                }
+            ]
             
         }
     },
